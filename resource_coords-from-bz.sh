d62fb869c8e914983d9b3b06b18059739fd03cff
lsb <leebutterman@gmail.com>
2016-12-25 10:56:55 -0800

    Read from stdin instead of a specific path

--- resource_coords-from-bz.sh
+++ resource_coords-from-bz.sh
@@ -1,1 +1,1 @@
-bzip2 -cd geo_coordinates_en.nt.bz2 | awk -v OFS="\t" '$2 == "<http://www.georss.org/georss/point>" { print $1, $3, $4 }' | sed -e 's/[.]$//' -e 's/"//g'
+bzip2 -cd /dev/stdin | awk -v OFS="\t" '$2 == "<http://www.georss.org/georss/point>" { print $1, $3, $4 }' | sed -e 's/[.]$//' -e 's/"//g'
