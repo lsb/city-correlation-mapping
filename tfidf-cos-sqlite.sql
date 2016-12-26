56d1f849c56aadbbd3b0d6016bff89f6e89cac43
lsb <leebutterman@gmail.com>
2016-12-25 16:14:10 -0800

    WIP use a table valued function for windowing

--- tfidf-cos-sqlite.sql
+++ tfidf-cos-sqlite.sql
@@ -1,4 +1,4 @@
--- pragma cache_size = 100000;
+pragma cache_size = 10000000;
 
 .mode tabs
 create table resource_ids (resource text, id integer primary key);
@@ -9,13 +9,13 @@ create table resource_coords (resource text, lat real, lng real);
 
 -- create table revisions (id integer primary key, epochtime int, user text, page_id int, page_title text); -- use parse-wikipedia-revisions to get this from the stub-meta-history
 
--- create table pages (id integer primary key, title text);
--- insert into pages select page_id, page_title from revisions on conflict ignore;
+create table pages (id integer primary key, title text);
+insert into pages select page_id, page_title from revisions on conflict ignore;
 
 create table page_coords (id integer primary key, lat real, lng real);
 insert into page_coords select id, lat, lng from resource_ids i join resource_coords c using (resource);
 
--- drop table resource_ids; drop table resource_coords; vacuum;
+drop table resource_ids; drop table resource_coords;
 
 create table users (id integer primary key, user text);
 insert into users (user) select user from revisions join page_coords on page_id = page_coords.id group by user order by count(*) desc;
@@ -28,7 +28,7 @@ insert into epochyears select distinct strftime("%Y", epochsecond, "unixepoch") 
 create table located_revisions (id integer primary key, epochsecond int, user_id int, page_id int);
 insert into located_revisions select r.id, epochsecond, u.id, page_id from revisions r join users u using (user) join page_coords p on r.page_id = p.id;
 
--- drop table revisions; vacuum;
+drop table revisions; vacuum; analyze;
 
 create table term_frequency (user_id int, page_id int, epochyear int, c int); -- term = user, epochyear = high water mark for visibility
 insert into term_frequency select user_id, page_id, epochyear, count(*) from located_revisions r join epochyears y on strftime("%Y", epochsecond, "unixepoch") <= epochyear group by user_id, page_id, epochyear;
@@ -48,11 +48,22 @@ insert into user_relevance_to_page_magnitudes select user_id, page_id, epochyear
 create index i1 on user_relevance_to_page_magnitudes (epochyear, user_id, page_id, tfidf, magnitude);
 create index i2 on user_relevance_to_page_magnitudes (epochyear, page_id, user_id, tfidf, magnitude);
 
-create table top_twenty_interpage_similarities (page1_id integer, page2_id integer, epochyear integer, similarity double precision);
-insert into top_twenty_interpage_similarities select a.page_id, b.page_id, epochyear, -log(sum((a.tfidf * b.tfidf) / (a.magnitude * b.magnitude))) similarity from user_relevance_to_page_magnitudes a join user_relevance_to_page_magnitudes b using (epochyear, user_id) where a.page_id = ? and epochyear = ? and a.page_id != b.page_id group by b.page_id order by similarity limit 20
+create table top_twenty_interpage_similarities_json (page1_id integer, epochyear integer, similarities json);
+insert into top_twenty_interpage_similarities_json
+  select p.id, y.epochyear, json_group_array(
+    select json_object(page2_id, similarity) from
+      (
+        select b.page_id as page2_id, -log(sum((a.tfidf * b.tfidf) / (a.magnitude * b.magnitude))) as similarity
+          from user_relevance_to_page_magnitudes a join user_relevance_to_page_magnitudes b using (epochyear, user_id)
+        where a.page_id = p.id and a.epochyear = y.epochyear and b.page_id != a.page_id
+        group by b.page_id order by similarity limit 20
+      )
+  ) from page_coords p, epochyears y;
 
-(def pages (map :id (clojure.java.jdbc/query ... ["select id from page_coords"])))
-(def epochyears (map :epochyear (clojure.java.jdbc/query ... ["select epochyear from epochyears"])))
-(for [y epochyears] (clojure.java.jdbc/with-db-transaction [txn ...] (doall (for [p pages] (clojure.java.jdbc/execute! txn ["insert into top_twenty_interpage_similarities select a.page_id, b.page_id, epochyear, -log(sum((a.tfidf * b.tfidf) / (a.magnitude * b.magnitude))) similarity from user_relevance_to_page_magnitudes a join user_relevance_to_page_magnitudes b using (epochyear, user_id) where a.page_id = ? and epochyear = ? and b.page_id != a.page_id group by b.page_id order by similarity limit 20" p y]))) nil))
+create table top_twenty_interpage_similarities (page1_id integer, page2_id integer, epochyear integer, similarity double precision);
+
+-- (def pages (map :id (clojure.java.jdbc/query ... ["select id from page_coords"])))
+-- (def epochyears (map :epochyear (clojure.java.jdbc/query ... ["select epochyear from epochyears"])))
+-- (for [y epochyears] (clojure.java.jdbc/with-db-transaction [txn ...] (doall (for [p pages] (clojure.java.jdbc/execute! txn ["insert into top_twenty_interpage_similarities select a.page_id, b.page_id, epochyear, -log(sum((a.tfidf * b.tfidf) / (a.magnitude * b.magnitude))) similarity from user_relevance_to_page_magnitudes a join user_relevance_to_page_magnitudes b using (epochyear, user_id) where a.page_id = ? and epochyear = ? and b.page_id != a.page_id group by b.page_id order by similarity limit 20" p y]))) nil))
 
 
