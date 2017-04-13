dd26cc3b571548ce3cd88bd250dff1edb8716dc0
lsb <leebutterman@gmail.com>
2017-04-13 10:08:21 -0700

    Tidy up

--- import-coords.sql
+++ import-coords.sql
@@ -8,4 +8,4 @@ create table resource_coords (resource text, lat real, lng real);
 create table page_coords (id integer primary key, lat real, lng real);
 insert into page_coords select id, lat, lng from resource_ids i join resource_coords c using (resource);
 
-drop table resource_ids; drop table resource_coords;
+drop table resource_ids; drop table resource_coords;vacuum;
