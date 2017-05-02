cd27a053948bb28d44276570ff9cc08c5feb41ad
lsb <leebutterman@gmail.com>
2017-05-01 17:02:47 -0700

    Update resource_coords-from-bz.sh
    Ensure `<urls>` are treated as separate tokens

--- resource_coords-from-bz.sh
+++ resource_coords-from-bz.sh
@@ -1,1 +1,1 @@
-bzip2 -cd /dev/stdin | awk -v OFS="\t" '$2 == "<http://www.georss.org/georss/point>" { print $1, $3, $4 }' | sed -e 's/[.]$//' -e 's/"//g'
+bzip2 -cd /dev/stdin | sed -e 's/</ </g' | awk -v OFS="\t" '$2 == "<http://www.georss.org/georss/point>" { print $1, $3, $4 }' | sed -e 's/[.]$//' -e 's/"//g'
